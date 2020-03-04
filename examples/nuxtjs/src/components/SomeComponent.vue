36ca27605c12664250cdf29db60402211395f763
Özüm Eldoğan <ozum@ozum.net>
2020-03-04 17:38:59 +0300

    update nuxt example

---
+++ examples/nuxtjs/src/components/SomeComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    HELLO
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "nuxt-property-decorator";
+
+@Component
+export default class SomeComponent extends Vue {
+  @Prop(Number) readonly propA: number | undefined
+}
+</script>
